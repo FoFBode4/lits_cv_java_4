19aa84d8474539364527ec4b3bce8682cbbf8550
Alex <fofbode4@gmail.com>
2020-08-20 19:25:45 +0300

    new

--- testapi/src/main/java/edu/lits/testapi/controller/ResumeController.java
+++ testapi/src/main/java/edu/lits/testapi/controller/ResumeController.java
@@ -1,6 +1,9 @@
 package edu.lits.testapi.controller;
 
 import edu.lits.testapi.model.Card;
+import edu.lits.testapi.pojo.Resume;
+import edu.lits.testapi.service.ResumeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,9 @@ import java.util.List;
 public class ResumeController {
     private  static  final int LOGGER_IN_USER_ID=1;
 
+    @Autowired
+    private ResumeService resumeService;
+
     @GetMapping("/item")
     @ResponseBody
 
@@ -21,6 +27,7 @@ public class ResumeController {
                         @RequestParam(required = false,
                                 defaultValue = "Chernivtsi") String location) {
         System.out.println("here");
+        Resume resume = resumeService.readByID(1L);
         return new Card();
     }
 
