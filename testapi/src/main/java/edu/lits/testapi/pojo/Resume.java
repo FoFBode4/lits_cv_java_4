6b81d7d6acf76ff6e93e03cb7506c3189e6c4083
Alex <fofbode4@gmail.com>
2020-08-27 18:04:01 +0300

    new commit

--- testapi/src/main/java/edu/lits/testapi/pojo/Resume.java
+++ testapi/src/main/java/edu/lits/testapi/pojo/Resume.java
@@ -1,4 +1,38 @@
 package edu.lits.testapi.pojo;
-
+import javax.persistence.*;
+@Entity
+@Table(name = "resume")
 public class Resume {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String user_id;
+    private  Long profession_id;
+
+    public Resume() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+
+    public Long getProfession_id() {
+        return profession_id;
+    }
+
+    public void setProfession_id(Long profession_id) {
+        this.profession_id = profession_id;
+    }
 }
--- testapi/src/main/java/edu/lits/testapi/service/ResumeServiceImpl.java
+++ testapi/src/main/java/edu/lits/testapi/service/ResumeServiceImpl.java
@@ -3,16 +3,19 @@ package edu.lits.testapi.service;
 import edu.lits.testapi.pojo.Resume;
 import edu.lits.testapi.repository.ResumeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class ResumeServiceImpl implements ResumeService {
     @Autowired
     private ResumeRepository resumeRepository;
 
     @Override
     public Resume readByID(Long id) {
-       Resume resume =  resumeRepository.getOne(1L);
-       return resume;
+       Optional <Resume> resume =  resumeRepository.findById(1L);
+       return resume.get();
     }
 }
