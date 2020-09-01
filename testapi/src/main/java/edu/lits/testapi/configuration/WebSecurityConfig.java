546ce25d306b1cc7dba20fc416798509aad43b8b
Alex <fofbode4@gmail.com>
2020-09-01 20:20:54 +0300

    Add security

--- testapi/src/main/java/edu/lits/testapi/configuration/WebSecurityConfig.java
+++ testapi/src/main/java/edu/lits/testapi/configuration/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package edu.lits.testapi.configuration;
 
+import edu.lits.testapi.controller.JwtAuthenticationEntryPoint;
+import edu.lits.testapi.controller.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,31 +26,36 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
    private UserDetailsService jwtUserDetailsService;
    @Autowired
    private JwtRequestFilter jwtRequestFilter;
+
    @Autowired
    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
       auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
    }
+
    @Bean
    public PasswordEncoder passwordEncoder() {
       return new BCryptPasswordEncoder();
    }
+
    @Bean
    @Override
    public AuthenticationManager authenticationManagerBean() throws Exception {
       return super.authenticationManagerBean();
    }
+
    @Override
    protected void configure(HttpSecurity httpSecurity) throws Exception {
       // We don't need CSRF for this example
       httpSecurity.csrf().disable()
-            // dont authenticate this particular request
-            .authorizeRequests().antMatchers("/authenticate").permitAll().
-            // all other requests need to be authenticated
-            anyRequest().authenticated().and().
-            // make sure we use stateless session; session won't be used to
-            // store user's state.
-            exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+              // dont authenticate this particular request
+              .authorizeRequests().antMatchers("/authenticate").permitAll().
+              // all other requests need to be authenticated
+                      anyRequest().authenticated().and().
+              // make sure we use stateless session; session won't be used to
+              // store user's state.
+                      exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+              .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
       // Add a filter to validate the tokens with every request
       httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
    }
+}
